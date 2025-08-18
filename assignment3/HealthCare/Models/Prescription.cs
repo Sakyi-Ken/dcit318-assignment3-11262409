9fad349960e0e4c7c87e05efe9f1b2baaf6ed7fd
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-08-18 11:07:22 +0000

    Add Prescription class to manage patient medication details

---
+++ assignment3/HealthCare/Models/Prescription.cs
@@ -0,0 +1,23 @@
+namespace Assignment3.Models
+{
+  public class Prescription
+  {
+    public int Id { get; }
+    public int PatientId { get; }
+    public string MedicationName { get; }
+    public DateTime DateIssued { get; }
+
+    public Prescription(int id, int patientId, string medicationName, DateTime dateIssued)
+    {
+      Id = id;
+      PatientId = patientId;
+      MedicationName = medicationName;
+      DateIssued = dateIssued;
+    }
+
+    public override string ToString()
+    {
+      return $"Prescription ID: {Id}, Patient ID: {PatientId}, Medication: {MedicationName}, Date Issued: {DateIssued}";
+    }
+  }
+}
