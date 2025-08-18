8964b64a3ed515c16f051ea87328c9927762a14a
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-08-18 12:45:48 +0000

    Refactor Program class  to clarify HealthSystemApp usage

--- assignment3/Program.cs
+++ assignment3/Program.cs
@@ -1,11 +1,14 @@
 ﻿using Assignment3.FinanceManagement.App;
+using Assignment3.App;
 
 class Program
 {
   static void Main(string[] args)
   {
     // var app = new FinanceApp();
-    FinanceApp app = new FinanceApp();
+    //FinanceApp app = new FinanceApp();
+
+    var app = new HealthSystemApp();
     app.Run();
   }
 }
