d11be1f06d52b3faaa070524224e79b0b785d612
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-08-19 13:22:07 +0000

    Add ElectronicItem and GroceryItem classes to represent inventory items

---
+++ assignment3/WarehouseInventory/Models/GroceryItem.cs
@@ -0,0 +1,23 @@
+namespace assignment3.WarehouseInventory.Models
+{
+  public class GroceryItem : IInventoryItem
+  {
+    public int Id { get; }
+    public string Name { get; }
+    public int Quantity { get; }
+    public DateTime ExpiryDate { get; }
+
+    public GroceryItem(int id, string name, int quantity, DateTime expiryDate)
+    {
+      Id = id;
+      Name = name;
+      Quantity = quantity;
+      ExpiryDate = expiryDate;
+    }
+
+    public override string ToString()
+    {
+      return $"[Grocery] ID: {Id}, Name: {Name}, Quantity: {Quantity}, Expiry: {ExpiryDate.ToShortDateString()}";
+    }
+  }
+}
