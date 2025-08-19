9abde6fdd1298d9eb775c46a8dcd371eeb6fd057
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-08-19 13:19:28 +0000

    Add InventoryRepository class for managing inventory items with CRUD operations

---
+++ assignment3/WarehouseInventory/Repositories/InventoryRepository.cs
@@ -0,0 +1,51 @@
+using assignment3.WarehouseInventory.Models;
+using assignment3.WarehouseInventory.Exceptions;
+
+namespace assignment3.WarehouseInventory.Repositories
+{
+  public class InventoryRepository<T> where T : IInventoryItem
+  {
+    private readonly Dictionary<int, T> _items = new Dictionary<int, T>();
+
+    public void AddItem(T item)
+    {
+      if (_items.ContainsKey(item.Id))
+        throw new DuplicateItemException($"Item with ID {item.Id} already exists.");
+
+      _items[item.Id] = item;
+    }
+
+    public T GetItemById(int id)
+    {
+      if (!_items.ContainsKey(id))
+        throw new ItemNotFoundException($"Item with ID {id} was not found.");
+
+      return _items[id];
+    }
+
+    public void RemoveItem(int id)
+    {
+      if (!_items.Remove(id))
+        throw new ItemNotFoundException($"Item with ID {id} was not found.");
+    }
+
+    public List<T> GetAllItems()
+    {
+      return new List<T>(_items.Values);
+    }
+
+    public void UpdateQuantity(int id, int newQuantity)
+    {
+      if (newQuantity < 0)
+        throw new InvalidQuantityException("Quantity cannot be negative.");
+
+      // if (!_items.ContainsKey(id))
+      //   throw new ItemNotFoundException($"Item with ID {id} was not found.");
+
+      //_items[id].Quantity = newQuantity;
+      
+      var item = GetItemById(id);
+      item.Quantity = newQuantity;
+    }
+  }
+}
