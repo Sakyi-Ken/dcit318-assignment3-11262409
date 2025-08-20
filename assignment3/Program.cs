9d4295ce27e5749ae82834ae971f9160708db857
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2025-08-20 10:33:10 +0000

    Refactor Program.cs to initialize InventoryApp

--- assignment3/Program.cs
+++ assignment3/Program.cs
@@ -2,6 +2,7 @@
 // using Assignment3.App;
 // using assignment3.WarehouseInventory.App;
 using assignment3.SchoolGradingSys.Processing;
+using assignment3.InventorySystem.App;
 
 class Program
 {
@@ -16,7 +17,11 @@ class Program
     // var app = new WareHouseManager();
     // app.Run();
 
-    var app = new StudentResultProcessor();
+    // var app = new StudentResultProcessor();
+    // app.Run();
+
+    string filePath = "inventory.json"; // persisted as JSON
+    var app = new InventoryApp(filePath);
     app.Run();
   }
 }
